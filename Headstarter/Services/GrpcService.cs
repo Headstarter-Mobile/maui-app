a18ba5b91775f69871c110913dcb130b2a2b7ff9
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2025-03-02 03:20:58 +0200

    Drop Models in favor of protobufs

---
+++ Headstarter/Services/GrpcService.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Net.Client;
+using Headstarter.Protos;
+
+namespace Headstarter.Services
+{
+    public sealed class GrpcService
+    {
+        private readonly GrpcChannel channel;
+        public readonly JobSearch.JobSearchClient jobSearchClient;
+        public readonly Users.UsersClient usersClient;
+
+        private static readonly GrpcService instance;
+
+        static GrpcService()
+        {
+            instance = new GrpcService();
+        }
+
+        private GrpcService()
+        {
+            channel = GrpcChannel.ForAddress("http://129.159.196.117:5001");
+            jobSearchClient = new JobSearch.JobSearchClient(channel);
+            usersClient = new Users.UsersClient(channel);
+        }
+
+        public static GrpcService Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
